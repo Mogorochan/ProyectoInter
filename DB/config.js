eecdada9423e163c67ffe31c07128e3ce550b64f
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-10-19 13:56:14 -0500

    back

--- DB/config.js
+++ DB/config.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() =>{
     try {
         await mongoose.connect(process.env.BD_CNN);
-        console.log('Arriba!');
+        console.log('¡Conectado!');
     } catch (error) {
         console.log(error);
         throw new Error('Error al iniciar la BD');
--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 const { response} = require('express');
 const  bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario =  async(req, res = response) => {
  
@@ -16,19 +17,24 @@ const crearUsuario =  async(req, res = response) => {
         }
         //Crear usuario con el modelo
         const dbUser = new Usuario(req.body);
-        
+
         //Encriptar la contraseña(por medio del hash)
         const salt = bcrypt.genSaltSync();
         dbUser.password = bcrypt.hashSync(password,salt);
 
+        //Generar el Jason web token (JWT)
+        const token = await generarJWT(dbUser.id, name);
+
         //Crear usuario en BD
         await dbUser.save();
         //Generar respuesta exitosa
+
         return res.status(201).json({
             ok: true,
             uid: dbUser.id,
             name,
-            email
+            email,
+            token
         })
 
         
@@ -45,21 +51,62 @@ const crearUsuario =  async(req, res = response) => {
 
 const loginUsuario = async(req, res = response) => {
 
-    return res.jason({
-        ok: true,
-        msg: ' usuario'
-    })
-    return res.status(400).json({
-        ok: false,
-        msg: "El email no es válido o no existe, favor revisárlo"
-    });
+    const { email, password} = req.body;
+
+    try {
+        const dbUser = await Usuario.findOne({email});
+
+        if (!dbUser) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El email no es válido o no existe, favor revisárlo"
+            });
+        }
+    //Confirmar si el password hace match
+        const validPassword = bcrypt.compareSync(password, dbUser.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: "La contraseña no es válida o no existe, favor revisárlo"
+            });
+        }
+    //Generar JWT
+        const token = await generarJWT(dbUser.id, dbUser.name);
+    //Respuesta del servicio
+        return res.json({
+            ok: true,
+            uid: dbUser.id,
+            name: dbUser.name,
+            email: dbUser.email,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: "Problemas al ingresar, contacte al administrador"
+        });
+    }
 }
 
 const revalidarToken =  async(req, res = response) => {
   
-    return res.status(400).json({
-        ok: false,
-        msg: "token"
+    const { uid} = req;
+
+    //Leer la BD
+    const dbUser = await Usuario.findById(uid);
+
+
+    // Generar el JWT
+    const token = await generarJWT( uid, dbUser.name );
+
+    return res.json({
+        ok: true,
+        uid, 
+        name: dbUser.name,
+        email: dbUser.email,
+        token
     });
 }
 
