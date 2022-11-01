2c5103717ad1fe492c6e35582e6530d5dffed4cc
Natalia <74684373+Mogorochan@users.noreply.github.com>
2022-11-01 09:46:45 -0500

    detalle estructura: hecho

--- BienesApp/src/app/rotacion/pages/detalle/detalle.component.ts
+++ BienesApp/src/app/rotacion/pages/detalle/detalle.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Rotacion } from '../../interfaces/rotacion';
+import { RotacionService } from '../../services/rotacion.service';
 
 @Component({
   selector: 'app-detalle',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleComponent implements OnInit {
 
-  constructor() { }
+  mostraRegistro!: Rotacion;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private rotacionService: RotacionService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.rotacionService.getRegistroPorID(id))
+      )
+      .subscribe (registro => this.mostraRegistro = registro);
+  }
+  regresar(){
+    this.router.navigate(['/rotacion/listado']);
   }
 
 }
